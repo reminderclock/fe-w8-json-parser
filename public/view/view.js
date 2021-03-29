4f5d8a3a0d1d4e4472e4e86881fab017471b4a5e
reminderclock <reminderclock@naver.com>
2021-03-29 21:27:02 +0900

    chore:git 충돌해결

--- public/view/view.js
+++ public/view/view.js
@@ -1,6 +1,6 @@
 import { _ } from "../src/util.js";
 const $root = _.$("#root");
-export default mainView() {
+export default function mainView() {
 //     return 
 //     `<div class="root__body-container">
 //     <div class=>
