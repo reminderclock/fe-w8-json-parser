549404b7ae4218ef67056c39c8bc8e6cc147a65c
reminderclock <reminderclock@naver.com>
2021-03-30 14:51:19 +0900

    feat: token ing

--- public/src/main.js
+++ public/src/main.js
@@ -1,69 +1,63 @@
 import '../scss/style.scss';
 import MainView from '../view/view';
 import packageJson from '../../package.json';
+import { inputTitle } from './const';
 
 const packageJsonData = JSON.stringify(packageJson);
 console.log(packageJsonData);
 
 const mainView = new MainView();
 
-const dataJson = [
-  '1a3',
-  [null, false, ['11', [112233], { easy: ['hello', { a: 'a' }, 'world'] }, 112], 55, '99'],
-  { a: 'str', b: [912, [5656, 33], { key: 'innervalue', newkeys: [1, 2, 3, 4, 5] }] },
-  true,
-];
-
-const jsonData = JSON.parse(JSON.stringify(dataJson));
-
-const parserObject = {
-  identifier: [],
-  keyword: [],
-  seperator: [],
-  operator: [],
-  literal: [],
-  comment: [],
-};
-
+const inputData = '["1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]';
 const tokenList = [];
-
+// console.log("" === '');
 function getTokenList(data) {
-  data.forEach((el) => {
-    console.log(el);
-    console.log(typeof el);
-    switch (typeof el) {
-      case 'string':
-      case 'number':
-      case 'boolean':
-      case 'null':
-        pushToken(el);
+  let splitData = data.split('');
+  splitData.forEach((el, idx) => {
+    switch (el) {
+      case '"':
+        getTokenString(el, idx);
         break;
-      case 'object':
-        checkObject(el);
-        break;
+      case 'n':
+        getTokenNull(el);
+      case 'f':
+      case 't':
+        getTokenBoolean(el);
+      case '[':
+        getTokenArr(el);
+      case '{':
+        getTokenObj(el);
       default:
         break;
     }
-  });
-  return tokenList;
-}
-
-function pushToken(data) {
-  tokenList.push(data);
-}
-
-function checkObject(data) {
-  if (data === null) return pushToken(data);
-  if (Array.isArray(data)) {
-    return getTokenList(data);
-  } else {
-    return getObjectToken(data);
   }
+  );
 }
 
-function getObjectToken(data) {
-  const objKey = Object.keys(data);
-  const objValue = Object.values(data);
-  getTokenList(objKey);
-  getTokenList(objValue);
+function getTokenArr(el) {
+  console.log(el);
 }
+function getTokenBoolean(el) {
+  console.log(el);
+}
+function getTokenObj(el) {
+  console.log(el);
+}
+function getTokenNull(el) {
+  console.log(el);
+}
+
+
+
+function getTokenString(data, idx) {
+  debugger;
+  if(data === '"') return;
+  let tokenString = '';
+  while(data !== '"') {
+    tokenString += data;
+    idx++;
+  }
+  return console.log(tokenString);
+}
+getTokenList(inputData);
+
